09b753604fd631ca222a38193ff7d76e9a1fcecd
nemo260 <nneemmeecc.m@gmail.com>
2022-03-25 11:20:43 +0100

    logout

--- db_connection/urls.py
+++ db_connection/urls.py
@@ -2,8 +2,10 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('v1/test', views.test, name="test"),
-    path('user/tasks', views.print_tasks, name="print_tasks"),
     path('user/login', views.login, name="login"),
-    path('user/marks', views.getUserMarks, name="getUserMarks")
+    path('user/logout', views.logout, name="logout"),
+
+    path('user/marks', views.getUserMarks, name="getUserMarks"),
+    path('user/tasks', views.print_tasks, name="print_tasks"),
+
 ]
--- db_connection/views.py
+++ db_connection/views.py
@@ -9,12 +9,20 @@ import environ
 
 env = environ.Env()
 environ.Env.read_env()
-
 conn = psycopg2.connect(database=env('db_name'), user=env('db_user'),
                         password=env('db_password'), host=env('db_host'),
                         port=env('db_port'))
-
 cur = conn.cursor()
+
+
+def loggedUser(request):
+    token = request.headers['Authorization'].split(' ')[1]
+    token = jwt.decode(token, 'secret', algorithms='HS256')
+
+    if token['id'] == 82548254:
+        return False
+    else:
+        return True
 
 
 def validateToken(request):
@@ -34,25 +42,59 @@ def getUserIdByToken(request):
     return token['id']
 
 
-def test(request):
-    query = "SELECT * from users"
+def login(request):
+    if request.method != 'POST':
+        return JsonResponse({'message': 'Bad request method'}, status=400)
+
+    body = request.body.decode('utf-8')
+    dict = json.loads(body)
+    query = "select * from users where email = '" + dict['email'] + "' and password = '" + dict['password'] + "'"
 
     cur.execute(query)
+    result = cur.fetchall()
 
-    return HttpResponse(cur)
+    if not result:
+        return JsonResponse({'message': 'Wrong password or email'}, status=400)
+
+    header = {
+        'id': result[0][0],
+        'exp': datetime.datetime.utcnow() + datetime.timedelta(hours=24),
+        'iat': datetime.datetime.utcnow()
+    }
+    token = jwt.encode(header, 'secret', algorithm='HS256').decode('utf-8')
+    return JsonResponse({'login token': token}, status=200)
 
 
-def print_tasks(request):
-
-    if request.method != 'GET':
-        return JsonResponse({'message': 'Bad request'}, status=400)
+def logout(request):
+    if request.method != 'POST':
+        return JsonResponse({'message': 'Bad request method'}, status=400)
 
     if not validateToken(request):
         return JsonResponse({'message': 'Bad token'}, status=401)
 
+    header = {
+        'id': 82548254,
+        'exp': datetime.datetime.utcnow() + datetime.timedelta(hours=24),
+        'iat': datetime.datetime.utcnow()
+    }
+    token = jwt.encode(header, 'secret', algorithm='HS256').decode('utf-8')
+    return JsonResponse({'logout token': token}, status=200)
+
+
+def print_tasks(request):
+    if request.method != 'GET':
+        return JsonResponse({'message': 'Bad request method'}, status=400)
+
+    if not validateToken(request):
+        return JsonResponse({'message': 'Bad token'}, status=401)
+
+    if not loggedUser(request):
+        return JsonResponse({'message': 'You are not logged in'}, status=401)
+
     user_id = getUserIdByToken(request)
 
-    query = "select u.id as user_id, u.name as name, u.surname as surname, t.name as task_name, t.subject as subject from users as u, tasks as t, user_tasks as ut where u.id = ut.user_id and t.id = ut.task_id and u.id = " + str(user_id)
+    query = "select u.id as user_id, u.name as name, u.surname as surname, t.name as task_name, t.subject as subject from users as u, tasks as t, user_tasks as ut where u.id = ut.user_id and t.id = ut.task_id and u.id = " + str(
+        user_id)
 
     cur.execute(query)
     result = cur.fetchall()
@@ -70,29 +112,6 @@ def print_tasks(request):
         })
 
     return JsonResponse(object, status=200)
-
-
-def login(request):
-    if request.method == 'POST':
-        body = request.body.decode('utf-8')
-        dict = json.loads(body)
-        query = "select * from users where email = '" + dict['email'] + "' and password = '" + dict['password'] + "'"
-
-        cur.execute(query)
-        result = cur.fetchall()
-
-        if not result:
-            return JsonResponse({'message': 'Wrong password or email'}, status=400)
-
-        header = {
-            'id': result[0][0],
-            'exp': datetime.datetime.utcnow() + datetime.timedelta(hours=24),
-            'iat': datetime.datetime.utcnow()
-        }
-        token = jwt.encode(header, 'secret', algorithm='HS256').decode('utf-8')
-        return JsonResponse({'token': token}, status=200)
-    else:
-        return JsonResponse({'message': 'Bad request'}, status=400)
 
 
 def getUserMarks(request):
