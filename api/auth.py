0ed34dc946b3806ca7bf58fd4b57c646908e7012
Mathey7 <xsabom2@stuba.sk>
2022-03-28 01:03:26 +0200

    new wrapper method for validating requests

--- api/auth.py
+++ api/auth.py
@@ -1,11 +1,61 @@
-import json, datetime
+import json
+import datetime
 import jwt
-
 from django.http import JsonResponse
+
+
+def check_request(request,
+                  request_method: str = None,
+                  must_be_logged_in: bool = False,
+                  must_be_teacher: bool = False,
+                  required_fields: list[str] = None,
+                  ) -> JsonResponse or None:
+    """
+    Checks request for stuff specified in the parameters
+
+    :parameter request: Request to be picked apart
+    :param request_method: Type of request (GET, POST, PUT...), if not specified, accepts any
+    :param must_be_logged_in: Whether the user must be logged in (True/False), default: False
+    :param must_be_teacher: Whether the user must have teacher privileges (True/False), default: False
+    :param required_fields: List of required fields in request.body json.
+    """
+    if request_method and request.method != request_method:
+        return JsonResponse({'message': 'Bad request method'}, status=400)
+
+    if must_be_logged_in and not validateToken(request):
+        return JsonResponse({'message': 'Bad token'}, status=400)
+
+    if must_be_logged_in and not loggedUser(request):
+        return JsonResponse({'message': 'You are not logged in'}, status=401)
+
+    if must_be_teacher and not is_user_teacher(request):
+        return JsonResponse({'message': 'You are not authorized for this action'}, status=403)
+
+    if required_fields:
+        if not request.body:
+            return JsonResponse({'message': 'No data'}, status=400)
+
+        data = request.body.decode('utf-8')
+        try:
+            data = json.loads(data)
+        except json.JSONDecodeError:
+            return JsonResponse({'message': 'Bad request'}, status=400)
+
+        # if anything is left in the checklist then something required is missing
+        checklist = required_fields.copy()
+        for item in required_fields:
+            if item in data:
+                checklist.remove(item)
+
+        if checklist:
+            return JsonResponse({'message': 'Missing required fields', 'Items': checklist}, status=400)
+
+    return None
+
+
 from api.db_connection import cur
 
 
-# check if user is logged in
 def loggedUser(request):
     token = request.headers['Authorization'].split(' ')[1]
     token = jwt.decode(token, 'secret', algorithms='HS256')
