0bec9fbfb860629afd99eca23e478694b707e2ed
nemo260 <nneemmeecc.m@gmail.com>
2022-03-27 19:43:20 +0200

    delete task + assign task to student

--- api/tasks.py
+++ api/tasks.py
@@ -83,30 +83,87 @@ def create_new_task(request):
     return JsonResponse({"message": "successfully added task"})
 
 
-#def delete_task(request, task_id):
-#    if request.method != 'DELETE':
-#        return JsonResponse({'message': 'Bad request method'}, status=400)
-#
-#    if not validateToken(request):
-#        return JsonResponse({'message': 'Bad token'}, status=401)
-#
-#    if not loggedUser(request):
-#        return JsonResponse({'message': 'You are not logged in'}, status=401)
-#
-#    if not is_user_teacher(request):
-#        return JsonResponse({'message': 'You are not authorized for this action'}, status=401)
-#
-#    cur.execute('''DELETE FROM tasks WHERE id = %s''', str(task_id))
-#    conn.commit()
-#
-#    cur.execute('''DELETE FROM user_tasks WHERE task_id = %s''', str(task_id))
-#    conn.commit()
-#
-#    cur.execute('''DELETE FROM marks WHERE task_id = %s''', str(task_id))
-#    conn.commit()
-#
-#    cur.execute('''DELETE FROM comments WHERE task_id = %s''', str(task_id))
-#    conn.commit()
-#
-#    return JsonResponse({"message": "successfully deleted task"}, status=200)
+def delete_task(request):
+    if request.method != 'DELETE':
+        return JsonResponse({'message': 'Bad request method'}, status=400)
+
+    if not validateToken(request):
+        return JsonResponse({'message': 'Bad token'}, status=401)
+
+    if not loggedUser(request):
+        return JsonResponse({'message': 'You are not logged in'}, status=401)
+
+    if not is_user_teacher(request):
+        return JsonResponse({'message': 'You are not authorized for this action'}, status=401)
+
+    if not request.body:
+        return JsonResponse({'message': 'No data'}, status=400)
+
+    data = json.loads(request.body.decode())
+
+    if 'task_id' not in data:
+        return JsonResponse({'message': 'No task id'}, status=400)
+
+    query = """select * from tasks where id = """ + str(data['task_id'])
+    cur.execute(query)
+    result = cur.fetchall()
+    if len(result) == 0:
+        return JsonResponse({'message': 'No task with this id'}, status=400)
+
+    cur.execute('''delete from user_tasks where task_id = %s''', (data['task_id'],))
+    conn.commit()
+
+    cur.execute('''delete from comments where task_id = %s''', (data['task_id'],))
+    conn.commit()
+
+    cur.execute('''delete from marks where task_id = %s''', (data['task_id'],))
+    conn.commit()
+
+    cur.execute('''DELETE FROM tasks WHERE id = %s''', (data['task_id'],))
+    conn.commit()
+
+    return JsonResponse({"message": "successfully deleted task"}, status=200)
+
+
+# assign task to student
+def assign_task(request):
+    if request.method != 'POST':
+        return JsonResponse({'message': 'Bad request method'}, status=400)
+
+    if not validateToken(request):
+        return JsonResponse({'message': 'Bad token'}, status=401)
+
+    if not loggedUser(request):
+        return JsonResponse({'message': 'You are not logged in'}, status=401)
+
+    if not is_user_teacher(request):
+        return JsonResponse({'message': 'You are not authorized for this action'}, status=401)
+
+    if not request.body:
+        return JsonResponse({'message': 'No data'}, status=400)
+
+    data = json.loads(request.body.decode())
+
+    if 'task_id' not in data:
+        return JsonResponse({'message': 'No task id'}, status=400)
+
+    if 'student_id' not in data:
+        return JsonResponse({'message': 'No student id'}, status=400)
+
+    query = """select * from tasks where id = """ + str(data['task_id'])
+    cur.execute(query)
+    result = cur.fetchall()
+    if len(result) == 0:
+        return JsonResponse({'message': 'No task with this id'}, status=400)
+
+    query = """select * from users where id = """ + str(data['student_id'])
+    cur.execute(query)
+    result = cur.fetchall()
+    if len(result) == 0:
+        return JsonResponse({'message': 'No student with this id'}, status=400)
+
+    cur.execute('''INSERT INTO user_tasks(user_id, task_id) VALUES (%s, %s)''', (data['student_id'], data['task_id']))
+    conn.commit()
+
+    return JsonResponse({"message": "successfully assigned task"}, status=200)
 
--- db_connection/urls.py
+++ db_connection/urls.py
@@ -1,7 +1,7 @@
 from django.urls import path
 
 from api.auth import login,logout
-from api.tasks import print_tasks, create_new_task #delete_task
+from api.tasks import print_tasks, create_new_task, delete_task, assign_task
 from api.marks import getUserMarks, assignMark, deleteMark
 from api.comments import get_comments_by_task_id, add_comment, delete_comment
 
@@ -13,7 +13,8 @@ urlpatterns = [
     path('user/tasks', print_tasks, name="print_tasks"),
 
     path('tasks/create', create_new_task, name='create_new_task'),
-    # path('tasks/delete/<int:task_id>', delete_task, name='delete_task'),
+    path('tasks/delete', delete_task, name='delete_task'),
+    path('tasks/assign', assign_task, name='assign_task'),
 
     path('comments/<int:task_id>', get_comments_by_task_id, name="get_comments_by_task_id"),
     path('comments/add/<int:task_id>', add_comment, name="add_comment"),
