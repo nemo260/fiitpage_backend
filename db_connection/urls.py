291f431b3f3bb014fb592beae49a70b8bd06e115
nemo260 <nneemmeecc.m@gmail.com>
2022-03-28 21:05:23 +0200

    new GET /user/class
    vypiše všetkych uživatelov v jednej triede/skupine
    využitie napriklad pre kontakty

--- db_connection/urls.py
+++ db_connection/urls.py
@@ -1,6 +1,6 @@
 from django.urls import path
 
-from api.auth import login,logout
+from api.auth import login,logout, get_users_from_class
 from api.tasks import print_tasks, create_new_task, delete_task, assign_task, update_task
 from api.marks import getUserMarks, assignMark, deleteMark, update_mark
 from api.comments import get_comments_by_task_id, add_comment, delete_comment
@@ -11,6 +11,7 @@ urlpatterns = [
 
     path('user/marks', getUserMarks, name="getUserMarks"),
     path('user/tasks', print_tasks, name="print_tasks"),
+    path('user/class', get_users_from_class, name="get_users_from_class"),
 
     path('tasks/create', create_new_task, name='create_new_task'),
     path('tasks/delete', delete_task, name='delete_task'),
